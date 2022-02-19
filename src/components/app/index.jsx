7c9570ad66f3325cbac110e1f9bb4030f71d0b18
a.miroshkin <a.miroshkin@jetmail.cc>
2022-02-19 12:50:50 +0300

    Разбил на компоненты и заверстал AppHeader

---
+++ src/components/app/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AppHeader from '../app-header'
+import Styles from './style.module.css';
+
+function App() {
+  return (
+    <div className={Styles.app}>
+      <AppHeader />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/app/index.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+// import { render, screen } from '@testing-library/react';
+import Index from './index';
+
+test('renders learn react link', () => {
+  render(<Index />);
+  // const linkElement = screen.getByText(/learn react/i);
+  // expect(linkElement).toBeInTheDocument();
+});
